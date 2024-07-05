1b2fa6831fd98c1d9e9aab9e0f473fd6d386931e
mahdi morovati <morovati155@gmail.com>
2024-07-05 17:05:48 +0330

    create ActorsController.Details

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -9,7 +9,7 @@ public class ActorsController(IActorsService  service) : Controller
     // GET
     public async Task<IActionResult> Index()
     {
-        var data = await service.GetAll();
+        var data = await service.GetAllAsync();
         return View(data);
     }
 
@@ -26,13 +26,25 @@ public class ActorsController(IActorsService  service) : Controller
     }
 
     [HttpPost]
-    public Task<IActionResult> Create([Bind("FullName,ProfilePictureUrl,Bio")]Actor actor)
+    public async Task<IActionResult> Create([Bind("FullName,ProfilePictureUrl,Bio")]Actor actor)
     {
         if (!ModelState.IsValid)
         {
-            return Task.FromResult<IActionResult>(View(actor));
+            return await Task.FromResult<IActionResult>(View(actor));
         }
-        service.Add(actor);
-        return Task.FromResult<IActionResult>(Redirect(nameof(Index)));
+        await service.AddAsync(actor);
+        return await Task.FromResult<IActionResult>(Redirect(nameof(Index)));
+    }
+    
+    // Get: Actors/Details/1
+    public async Task<IActionResult> Details(int id)
+    {
+        var actorDetails = await service.GetByIdAsync(id);
+        if (actorDetails == null)
+        {
+            return View("Empty");
+        }
+
+        return View(actorDetails);
     }
 }
--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -5,21 +5,22 @@ namespace eTickets.Data.Services;
 
 public class ActorsService(AppDbContext context) : IActorsService
 {
-    public async Task<IEnumerable<Actor>> GetAll()
+    public async Task<IEnumerable<Actor>> GetAllAsync()
     {
         var result = await context.Actors.ToListAsync();
         return result;
     }
-
-    public Actor GetById(int id)
+    
+    public async Task<Actor> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var result = await context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+        return result;
     }
 
-    public void Add(Actor actor)
+    public async Task AddAsync(Actor actor)
     {
-        context.Actors.Add(actor);
-        context.SaveChanges();
+        await context.Actors.AddAsync(actor);
+        await context.SaveChangesAsync();
     }
 
     public Actor Update(int id, Actor newActor)
